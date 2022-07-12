a781c7c63bc7b0b99b8ded2e39970bb4508fb427
Mehakk Lunkar <109075370+mehakklunkar@users.noreply.github.com>
2022-07-12 15:38:34 +0530

    Add files via upload

---
+++ adt.bat
@@ -0,0 +1,3 @@
+set BABEL_DATADIR=C:\Users\DELL\Desktop\OpenBabel-2.3.2\data
+set path=C:\Users\DELL\Desktop\OpenBabel-2.3.2;%path%
+"C:\Users\DELL\Desktop\python.exe" "C:\Users\DELL\Desktop\Lib\site-packages\AutoDockTools\bin\runAdt.py" %1 %2 %3 %4 %5 %6 %7 %8 %9
